ebe5565df45f929a67174d0fa9082bb60a7a8de6
anish523 <anishkh9374@gmail.com>
2025-02-20 20:18:32 +0000

    variables done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,18 @@
+const accountId = 11456
+let accountEmail = "anishkh9374@gmail.com"
+var accountPassword = "1245"
+accountCity = "jaipur"
+// accountId =2
+console.log(accountId);
+accountEmail = "anisuja@gmail.com"
+accountPassword = "447"
+accountCity = "delhi"
+let accountState
+
+//console.log(accountCity);
+//console.log(accountEmail);
+//console.log(accountPassword);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+/* not used var
+*/
+
