42c364394ced7dc0958a8698d17a5aefd8a64691
Anishujama khan <anishkh9374@gmail.com>
2025-02-21 16:55:21 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as newer version
+
+ // alert("hello")
+ // alert(4+4) // we are using nodejs, not browser
+
+ console.log(3+
+    33); // code readability should be high
+ console.log("Anish");
+
+ let name = "Anish"
+ let age = 20
+ let isLoggedIn = false
+ let state;
+
+ // some data types
+ // number => 2 to power 53
+ // bigInt
+ // string => ""
+ // boolean => true/false
+ // null => standalone Value
+ // undefined => 
+ // symbol => unique
+ // object 
+
+ console.log(typeof undefined); // undefined
+ console.log(typeof null); // object 
+ 
+ 
