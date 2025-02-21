2eba2c4a42ed20697ff0978b908ba88f8921a7d1
Anishujama khan <anishkh9374@gmail.com>
2025-02-21 17:19:19 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,30 @@
+let score = "anish"
+
+//const {score} = req.body
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(valueInNumber);
+
+// "33"=> 33
+// "33abc0 "=> Nan
+// true => 1; false => 0
+
+let isLoggedIn = "Anish"
+let booleanisLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn);
+
+// 1=> true
+// 0=> false
+// ""=> false
+// "Anish"=> true
+
+
+let someNumber = 25
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
