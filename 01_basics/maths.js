a0438933e55f2d4cc25a303053ca7351bf0b9ce9
Anishujama khan <anishkh9374@gmail.com>
2025-02-22 17:51:27 +0000

    math

---
+++ 01_basics/maths.js
@@ -0,0 +1,37 @@
+const score = 1003
+// console.log(score);
+const balance = new Number(122)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+// console.log(balance.toFixed(2));
+const otherNumber = 1124.5878
+// console.log(otherNumber.toPrecision(5));
+
+const hundred = 1000000
+// console.log(hundred.toLocaleString('en-IN'));
+
+// console.log(Math);
+// console.log(Math);
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,9,2,3,4,5,6));
+// console.log(Math.max(4,9,8));
+
+
+
+console.log(Math.random());
+console.log(Math.random()*10+1);
+console.log(Math.floor(Math.random()*10 + 1));
+
+const min = 10
+const max = 20
+
+console.log(Math.random(Math.random() *(max-min +1) + min))
+
+
+
+
+
