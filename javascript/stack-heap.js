5de80f6d63aa97680cb9b995d869f5c8ee4ec897
Anishujama khan <anishkh9374@gmail.com>
2025-02-22 14:13:52 +0000

    stack heap

---
+++ javascript/stack-heap.js
@@ -0,0 +1,8 @@
+// memory 
+// stack & heap 
+// In stack prmitive data types are used
+// In heap reference data types are used
+
+let myYoutubename = "cosmosfact"
+let anothername = myYoutubename
+console.log(anothername);
