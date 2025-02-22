5df8a20aeecf3df2bcf0e3e73709ea3deba606b8
Anishujama khan <anishkh9374@gmail.com>
2025-02-22 14:03:35 +0000

    datatypes

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,39 @@
+// primitve data types
+// 7  types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+// non primtive / reference 
+// Array, Objects, Functions
+
+//JavaScript is a dynamically typed language,
+
+const score = 100
+const scores  = 100.3
+const isLoggedIn = false
+const outSideTemp = null
+
+
+let userEmail = undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+//console.log(id=== anotherId);
+
+// const bigNumber = 48548745578n
+
+const heros = [ "shaktiman", "nagraaj", "doga"];
+let myObj = {
+
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction= function(){
+   // console.log("hello world");
+}
+
+console.log(typeof anotherId );
+
+
+
+
