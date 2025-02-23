814030d4a961a30adc8a8a2aec00eb7f6c510649
Anishujama khan <anishkh9374@gmail.com>
2025-02-23 13:02:46 +0000

    done

---
+++ 02_basics/object.js
@@ -0,0 +1,21 @@
+// singleton 
+
+// objects literals 
+// Object.create 
+const JsUser = { 
+    'full name ' : 'anishujama',
+name : 'anish',
+age : 20,
+location : 'jaipur',
+email : 'anish@gmail.com',
+isloggesIn : false,
+lastLoginDays : ['monday', 'saturday']
+
+}
+
+
+console.log(JsUser.email);
+console.log(JsUser['email']);
+console.log(JsUser["full name" ]);
+
+
