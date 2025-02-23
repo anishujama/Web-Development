47db3e42c0aeb95547cc155f7c9dc65ef2cd1b2e
Anishujama khan <anishkh9374@gmail.com>
2025-02-23 11:55:20 +0000

    array

---
+++ 02_basics/array.js
@@ -0,0 +1,34 @@
+const myArr = [ 0,1,2,3,4,5, true, 'anish']
+const myHeros = ['che guvera']
+const myArr2 = new Array(1,2,3,4)
+//console.log(myArr[1]);
+
+// myArr.unshift(9)
+//myArr.shift()
+// myArr.push(10)
+// myArr.pop()
+
+//console.log(myArr.includes(8));
+// console.log(myArr.indexOf(2));
+
+//const  newArr = myArr.join()
+// console.log(typeof newArr);
+
+// console.log(myArr);
+
+console.log('A', myArr);
+
+const myn1= myArr.slice(1, 3)
+console.log(myn1);
+
+console.log('b', myArr);
+const myn2 = myArr.splice(1,3)
+console.log(myn2);
+console.log('c', myArr);
+
+
+
+
+
+
+
