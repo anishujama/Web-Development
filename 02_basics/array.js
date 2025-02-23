c59b3468aa1818ad89082015b1fa757df7cea4b2
Anishujama khan <anishkh9374@gmail.com>
2025-02-23 18:13:28 +0000

    array

--- 02_basics/array.js
+++ 02_basics/array.js
@@ -16,15 +16,50 @@ const myArr2 = new Array(1,2,3,4)
 
 // console.log(myArr);
 
-console.log('A', myArr);
+// console.log('A', myArr);
 
 const myn1= myArr.slice(1, 3)
-console.log(myn1);
+// console.log(myn1);
 
-console.log('b', myArr);
+// console.log('b', myArr);
 const myn2 = myArr.splice(1,3)
-console.log(myn2);
-console.log('c', myArr);
+// console.log(myn2);
+// console.log('c', myArr);
+
+
+
+
+const marvel_heros = [ "A", "B","c"]
+const dc = [ "'superman'", "'flash'", 'batman']
+
+//marvel_heros.push(dc)
+// console.log(marvel_heros);
+
+// const allHeros = marvel_heros.concat(dc)
+// console.log(allHeros);
+
+const all_new_heros = [...marvel_heros,...dc]
+
+//console.log(all_new_heros);
+
+const another_array = [1,2,3,4,[5,6,7,],6,5,[7,5]]
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
+
+console.log(Array.isArray('anish'))
+console.log(Array.from('anish'))
+console.log(Array.from({name: 'anish'})) // interestng
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3));
+
+
+
+
 
 
 
