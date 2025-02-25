6f8ce7bdcb3c69b27f0202a130e4fe69521210a7
Anishujama khan <anishkh9374@gmail.com>
2025-02-25 18:44:58 +0000

    variables

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -1,18 +1,56 @@
-const accountId = 11456
-let accountEmail = "anishkh9374@gmail.com"
-var accountPassword = "1245"
-accountCity = "jaipur"
-// accountId =2
-console.log(accountId);
-accountEmail = "anisuja@gmail.com"
-accountPassword = "447"
-accountCity = "delhi"
-let accountState
+// const accountId = 11456
+// let accountEmail = "anishkh9374@gmail.com"
+// var accountPassword = "1245"
+// accountCity = "jaipur"
+// // accountId =2
+// console.log(accountId);
+// accountEmail = "anisuja@gmail.com"
+// accountPassword = "447"
+// accountCity = "delhi"
+// let accountState
 
-//console.log(accountCity);
-//console.log(accountEmail);
-//console.log(accountPassword);
-console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
-/* not used var
-*/
+// //console.log(accountCity);
+// //console.log(accountEmail);
+// //console.log(accountPassword);
+// console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+// /* not used var
+// */
+// console.log('hey developer world');
+
+// var a = 5;
+// var b = 6;
+// var c = 'Anish';
+
+// console.log(a + b + 8);
+// console.log(typeof a, typeof b, typeof c);
+// const a1 = 5;
+// // a1= a1+1;
+// // console.log(a1);  not allowed
+
+let x = "Anish";
+let y = 22;
+let z= 3.55;
+let p = true;
+let q = undefined
+let r = null;
+
+console.log(x,y,z,p,q,r);
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r);
+
+let o = {
+     name: "Anish",
+     "job code ": 5643,
+     "is_handsome" : true
+
+}
+console.log(o);
+o.salary = "100 cr"
+console.log(o);
+o.salary = "500 cr";
+
+
+
+
+
+
 
