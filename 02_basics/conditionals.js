ccf86b8f118045a442677ae51ba430239e294737
Anishujama khan <anishkh9374@gmail.com>
2025-02-25 19:31:38 +0000

    conditional

---
+++ 02_basics/conditionals.js
@@ -0,0 +1,72 @@
+// console.log("conditional");
+
+let age = 20;
+// let grace = 3;
+
+// age+= grace
+// console.log(age);
+
+// console.log(age + grace);
+// console.log(age - grace);
+// console.log(age * grace);
+// console.log(age / grace);
+// console.log(age ** grace);
+// console.log(age % grace);
+
+if (age>18)
+{
+    // console.log("you can derive");
+    
+}
+else
+{
+    // console.log('you cant derive');
+    
+}
+
+if(age==0)
+{
+    // console.log("you can drive");
+}
+else if(age==2)
+ {
+    // console.log("are you comedy me")
+}
+else
+{
+    // console.log("you can't drive");
+}
+
+
+if(age==0)
+{
+    //  console.log("you can drive");
+}
+else if(age==21)
+ {
+    //  console.log("are you comedy me")
+}
+else if(age==20)
+ {
+    //  console.log("are  you again comedy me")
+}
+else
+{
+    //  console.log("you can't drive");
+}
+
+
+a= 6;
+b=7;
+let c = a>b?(a-b):(b-a);
+
+// console.log(c);
+
+/*
+translate to: 
+if(a>b){
+let c = a-b;
+}
+else {
+let c = b-a;
+} */
