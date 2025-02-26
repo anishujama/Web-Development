dc03b328d7a857a1929983c99cb96f9fa829d87e
Anishujama khan <anishkh9374@gmail.com>
2025-02-26 18:49:33 +0000

    Name generator

---
+++ nameGenerator/name.js
@@ -0,0 +1,18 @@
+/*Create a business name generator by combining list of adjectives and shop name and aother word 
+Adjective:
+Crazy
+Amazing
+Fire
+
+Shop Name:
+Engine
+Foods
+Garments
+
+Another Word:
+bros
+Limited
+Hub
+
+*/
+
