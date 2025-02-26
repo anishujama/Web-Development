fbc70964dcfa8acc565239641b78ed7c6bf622f8
Anishujama khan <anishkh9374@gmail.com>
2025-02-26 16:28:06 +0000

    string

---
+++ 02_basics/string.js
@@ -0,0 +1,38 @@
+console.log("string");
+
+let a = "Anish"
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a.length);
+
+let b = "ahmad"
+console.log(b.toUpperCase());
+console.log(b.toLowerCase());
+console.log(b.length);
+console.log(b.slice(0,4));
+console.log(b.slice(1));
+
+
+const name = "anish"
+const repoCount = 5
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+console.log(b.replace("ad", "65"));
+
+console.log(b.concat(a,"aish", "rahul", "priya"));
+
+console.log(b);
+
+console.log("har\"".length);
+
+console.log(b.includes("a"));
+console.log(b.endsWith("d"));
+console.log(b.startsWith("a"));
+
+let c = "HAIDAR"
+
+console.log(c.toLowerCase());
+
