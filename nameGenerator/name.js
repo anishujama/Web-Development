8b920f0414ef25d8573b0390ab4eb06d9c63f86d
Anishujama khan <anishkh9374@gmail.com>
2025-03-01 12:44:13 +0530

    Update name.js

--- nameGenerator/name.js
+++ nameGenerator/name.js
@@ -15,4 +15,46 @@ Limited
 Hub
 
 */
+let rand = Math.random()
+        let first, second, third
+        // 0 0.33 0.66 1
+
+        // lets generate the first word
+
+        if(rand<0.33){
+            first = "Crazy"
+        }
+        else if(rand<0.66 &&rand>=0.33){
+            first = "Amazing"
+        }
+        else{
+            first = "Fire"
+        }
+         // lets generate the first word
+          rand = Math.random()
+
+         if(rand<0.33){
+            second = "engine"
+        }
+        else if(rand<0.66 &&rand>=0.33){
+            second = "foods"
+        }
+        else{
+            second = "garments"
+        }
+         // lets generate the third word
+          rand = Math.random()
+
+         if(rand<0.33){
+            third = "bros"
+        }
+        else if(rand<0.66 &&rand>=0.33){
+            third = "limited"
+        }
+        else{
+            third = "hub"
+        }
+        console.log(`${first} ${second} ${third}`)
+
+
 
