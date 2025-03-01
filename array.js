01830f75c6c3963cdea1f526bfc166e931622452
Anishujama khan <anishkh9374@gmail.com>
2025-03-01 12:17:42 +0530

    Add files via upload
    done

---
+++ array.js
@@ -0,0 +1,54 @@
+// let arr = [1,2,3,4,5]
+
+// arr[0]=4525
+// console.log(arr);
+// console.log(arr.length);
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+
+// console.log(arr.toString());
+// console.log(arr.join(' and '));
+
+// a.pop()
+// console.log(a);
+
+// a.push('anish')
+// a.push(100)
+// console.log(a);
+
+// a.shift()
+// console.log(a);
+
+// a.unshift("anish")
+// console.log(a);
+
+// delete a[5]
+// console.log(a);
+// a.length
+
+// let a1 = [1,2,3]
+// let a2 =[4,5,6]
+// let a3 =[7,8,9]
+
+// a1.concat(a2,a3)
+
+// let a1 = [7,4,5,2,9]
+// a.sort()
+
+// console.log(a1);
+let a = [1,2,3,4,5,6]
+// index = [0,1,2,3,4,5]
+a.splice(0,3,25,26);
+console.log(a);
+
+
+
+
+
+
+
+
+
+
